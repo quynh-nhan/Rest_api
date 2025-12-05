006da1ac7ac1de9edb9ca9a329d61f4cb74e2a04
nhantruong <139199716+quynh-nhan@users.noreply.github.com>
2025-12-05 21:48:49 +0700

    Update productRoter.js

--- routes/productRoter.js
+++ routes/productRoter.js
@@ -58,7 +58,7 @@ var sendMail= require("../utils/Mail");
   router.get("/all", async function(req,res) {
     // const list = await productRouter.find({},"name description price").populate("cateID");
     // const list = await productRouter.find({$and :[{price:{$gt:20000}},{quantity:{$gt:10}}]});
-    const list = await productRouter.find();
+    const list = await productRouter.find().limit(1);
     res.status(200).json({status:true,message:"thanh cong",data:list})
   });
 
